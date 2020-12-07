cc0163be26f59faa96f98534fd393e60cbeae70b
Lalajoy <58143432+helloweizhang@users.noreply.github.com>
2020-12-08 00:19:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,15 +26,6 @@ sed -i 's/luci-theme-bootstrap/luci-theme-opentomcat/g' feeds/luci/collections/l
 sed -i 's/%D %V, %C/OpenWrt SML v1.1[2021] | by "LuckyGoldVenus.Wechat"/g' package/base-files/files/etc/banner
 # rm -rf package/base-files/files/etc/banner && cp -f ../banner package/base-files/files/etc/
 
-# Add kernel build user
-[ -z $(grep "CONFIG_KERNEL_BUILD_USER=" x86_64.config) ] &&
-    echo 'CONFIG_KERNEL_BUILD_USER="luckygoldvenus@wechat"' >>x86_64.config ||
-    sed -i 's@\(CONFIG_KERNEL_BUILD_USER=\).*@\1$"luckygoldvenus"@' x86_64.config
-
-# Add kernel build domain
-[ -z $(grep "CONFIG_KERNEL_BUILD_DOMAIN=" x86_64.config) ] &&
-    echo 'CONFIG_KERNEL_BUILD_DOMAIN="GitHub Actions"' >>x86_64.config ||
-    sed -i 's@\(CONFIG_KERNEL_BUILD_DOMAIN=\).*@\1$"GitHub Actions"@' x86_64.config
 
 # 打开旁路由防火墙规则，旁路由使用取消注释号#
 echo "# iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
