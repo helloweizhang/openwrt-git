9a4d5a648754d0f9876514b439a5f98a4fdb320b
Lalajoy <58143432+helloweizhang@users.noreply.github.com>
2020-12-09 16:51:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # sed -i '$a src-git kenzok https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-sed -i '$a src-git 281677160 https://github.com/281677160/openwrt-package' feeds.conf.default
+sed -i '$a src-git beewinds https://github.com/281677160/openwrt-package' feeds.conf.default
